70bcfccc823b891550e771a6d9d9dc8061771f55
Nosherwan Babar <82045475+Nosher007@users.noreply.github.com>
2024-02-20 15:39:59 +0500

    registration component done

--- server/src/controllers/Register.controller.js
+++ server/src/controllers/Register.controller.js
@@ -1,26 +1,34 @@
 import { validationResult } from "express-validator";
 import { jsonGenerate } from "../utils/helper.js";
-import { statusCode } from "../utils/constant.js";
+import { statusCode, JWT_TOKEN_SECRET } from "../utils/constant.js";
 import bcrypt from 'bcrypt';
 import User from '../models/user.js'; // Import the User model
+import Jwt from 'jsonwebtoken';
 
 const Register = async (req, res) => {
     const errors = validationResult(req);
     if (errors.isEmpty()) {
         const { name, username, password, email } = req.body;
         const salt = await bcrypt.genSalt(10);
-        const hashPassword = await bcrypt.hash(password, salt); // Fix typo here
+        const hashPassword = await bcrypt.hash(password, salt);
 
-        // Save to Db
+        const userExist = await User.findOne({$or:[{ email }, { username }]});
+
+        if (userExist) {
+            return res.json(jsonGenerate(statusCode.UNPROCESSABLE_ENTRY, 'User or Email already exists'));
+        }
+
         try {
             const result = await User.create({
-                name: name,
-                email: email,
+                name,
+                email,
                 password: hashPassword,
-                username: username
+                username
             });
 
-            res.json(jsonGenerate(statusCode.SUCCESS, "Registration successful", result));
+            const token = Jwt.sign({ userId: result._id }, JWT_TOKEN_SECRET);
+
+            res.json(jsonGenerate(statusCode.SUCCESS, "Registration successful", { user: result, token }));
         } catch (error) {
             console.log(error);
             res.status(500).json(jsonGenerate(statusCode.INTERNAL_SERVER_ERROR, "Server error"));
--- server/src/utils/constant.js
+++ server/src/utils/constant.js
@@ -1,5 +1,9 @@
-export const DB_CONNECT="mongodb://localhost:27017/"
+export const DB_CONNECT="mongodb://localhost:27017/user"
+
+export const JWT_TOKEN_SECRET="jadsfjashdfjhasdjfh";
+
 export const statusCode={
     SUCCESS:200,
-    VALIDATION_ERROR:201
+    VALIDATION_ERROR:201,
+    UNPROCESSABLE_ENTERy:202
 }
