f647b62f55694232e89c4b0f9e65e255cc586565
Nosherwan Babar <82045475+Nosher007@users.noreply.github.com>
2024-02-22 12:47:59 +0500

    sexure login implemented

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { login } from '../services/api';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import { useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -11,33 +11,32 @@ function Login() {
     });
 
     const [errors, setErrors] = useState({});
-    const navigate = useNavigate(); // useNavigate hook
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        // Check if user is already logged in
+        const info = localStorage.getItem('user');
+        if (info) {
+            navigate('/'); // Redirect to home if user is already logged in
+        }
+    }, [navigate]);
 
     const handleChange = (e) => {
         setForm({ ...form, [e.target.name]: e.target.value });
     };
 
     const handleSubmit = async (e) => {
-        e.preventDefault(); // Prevent default form submission
-        console.log('form', form);
-        // Here you can add your login logic, for now just logging the form data
+        e.preventDefault();
         const result = await login(form);
-        console.log("form", result);
-        setErrors({}); // Reset errors
+        setErrors({});
 
         if (result.status === 200) {
-          
-            if (result.status === 200) {
-                localStorage.setItem('user', JSON.stringify(result.data.data));
-                navigate('/'); // Use navigate to redirect
-            }
+            localStorage.setItem('user', JSON.stringify(result.data.data));
+            navigate('/');
         } else if (result.status === 201) {
-            // Handle validation errors
             setErrors(result.data.data);
             toast.error(result.message);
-
         } else {
-            // Handle other HTTP status codes or network errors
             toast.error(result.message);
         }
     };
@@ -55,7 +54,6 @@ function Login() {
                                         <label htmlFor="exampleInputEmail1" className="form-label mt-4">Username</label>
                                         <input type="text" onChange={handleChange} name="username" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter username" style={{ maxWidth: '300px', margin: '0 auto' }} />
                                         {errors?.username && <small className="form-text text-muted">{errors.username.message}</small>}
-                                        
                                     </div>
                                     <div className="form-group">
                                         <label htmlFor="exampleInputPassword1" className="form-label mt-4">Password</label>
